39b88d913f8dc9d13e21eea7635938e7d164795c
unknown <bradyrcotton@gmail.com>
2021-03-12 10:48:00 -0600

    replaced function to throw filtered data into a table on the page and added a for loop to said function to add everyone in filteredPeople

--- app.js
+++ app.js
@@ -7,12 +7,38 @@ function searchByName(){
 
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
     let filteredPeople = people.filter(function (person) {
-        if(person.firstName === firstNameInput && person.lastName === lastNameInput){
+        if(person.firstName === firstNameInput || person.lastName === lastNameInput){
             return true;
         }
         return false;
     });
-  
+    function addRow(){
+        console.log(filteredPeople);
+        for (let i=0; i<filteredPeople.length; i++){
+        let tableRef = document.getElementById("myTable");
+        let tableRow1 = tableRef.insertRow(1);
+        let newCell1 = tableRow1.insertCell(0);
+        let newCell2 = tableRow1.insertCell(1);
+        let newCell3 = tableRow1.insertCell(2);
+        let newCell4 = tableRow1.insertCell(3);
+        let newCell5 = tableRow1.insertCell(4);
+        let firstNames = document.createTextNode(filteredPeople[i].firstName);
+        let lastNames =document.createTextNode(filteredPeople[i].lastName);
+        let personAge = document.createTextNode(filteredPeople[i].age);
+        let personHeight =document.createTextNode(filteredPeople[i].height);
+        let personWeight =document.createTextNode(filteredPeople[i].weight)
+        newCell1.appendChild(firstNames);
+        newCell2.appendChild(lastNames);
+        newCell3.appendChild(personAge);
+        newCell4.appendChild(personHeight);
+        newCell5.appendChild(personWeight);
+        }
+    }
+    addRow();
+    
+
+}
+
     // for(let i=0;i<filteredPeople.length;i++){
     //     console.log(filteredPeople["firstName"]);
     // }
@@ -30,12 +56,12 @@ function searchByName(){
     // }else{
     //     alert('Sorry, looks like there is no one with that name.');
     // }
-    function addRow(myTable){
-        let tableRef = document.getElementById("myTable");
-        let newRow = tableRef.insertRow(1);
-        let newCell = newRow.insertCell(0);
-        let newText = document.createTextNode(filteredPeople[0].firstName);
-        newCell.appendChild(newText);
-    }
-    addRow(myTable);
-}
+//     function addRow(myTable){
+//         let tableRef = document.getElementById("myTable");
+//         let newRow = tableRef.insertRow(1);
+//         let newCell = newRow.insertCell(0);
+//         let newText = document.createTextNode(filteredPeople[0].firstName);
+//         newCell.appendChild(newText);
+//     }
+//     addRow(myTable);
+// }
---
+++ data.js
@@ -0,0 +1,101 @@
+const people = [
+	{
+		"id": 272822514,
+		"firstName": "Billy",
+		"lastName": "Bob",
+		"age": 30,
+		"height": 71,
+		"weight": 175,
+	},
+	{
+		"id": 401222887,
+		"firstName": "Uma",
+		"lastName": "Bob",
+		"age": 30,
+		"height": 65,
+		"weight": 162,
+	},
+	{
+		"id": 409574486,
+		"firstName": "Michael",
+		"lastName": "Walkens",
+		"age": 60,
+		"height": 76,
+		"weight": 250,
+	},
+	{
+		"id": 260451248,
+		"firstName": "Jon",
+		"lastName": "Walkens",
+		"age": 70,
+		"height": 62,
+		"weight": 115,
+	},
+	{
+		"id": 629807187,
+		"firstName": "Jack",
+		"lastName": "Pafoy",
+		"age": 80,
+	},
+	{
+		"id": 464142841,
+		"firstName": "Jen",
+		"lastName": "Pafoy",
+		"age": 70,
+		"height": 72,
+		"weight": 256,
+	},
+	{
+		"id": 982411429,
+		"firstName": "Mister",
+		"lastName": "Potatoo",
+		"age":65,
+		"height": 66,
+		"weight": 170,
+	},
+	{
+		"id": 595767575,
+		"firstName": "Missuz",
+		"lastName": "Potatoo",
+		"age": 75,
+		"height": 59,
+		"weight": 137,
+	},
+	{
+		"id": 693243224,
+		"firstName": "Joy",
+		"lastName": "Madden",
+		"age": 40,
+		"height": 69,
+		"weight": 199,
+	},
+	{
+		"id": 888201200,
+		"firstName": "Mader",
+		"lastName": "Madden",
+		"age": 30,
+		"height": 76,
+		"weight": 205,
+	}
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
