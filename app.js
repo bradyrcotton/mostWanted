5979f59c65676c88ff20f83820ef54b6e6547baf
unknown <bradyrcotton@gmail.com>
2021-03-12 14:27:53 -0600

    added fuction to remove unwanted data from the table and replace it with only the data searched for

--- app.js
+++ app.js
@@ -64,6 +64,10 @@ function searchByName(){
         else{ return false;}
     });
 
+//     function deleteRows(){
+//         if
+//     }
+// }
     // for(let i=0;i < filteredPeople.length;i++){
     //     if (tableRef[i] != filteredPeople[i]){
 
@@ -71,47 +75,54 @@ function searchByName(){
     //     document.getElementById("myTable").deleteRow(i);
     //     }
     // }
-    // function addRow(){
-    //     console.log(filteredPeople);
-    //     for (let i=0; i<filteredPeople.length; i++){
-    //     let tableRef = document.getElementById("myTable");
-    //     let tableRow1 = tableRef.insertRow(1);
-    //     let newCell1 = tableRow1.insertCell(0);
-    //     let newCell2 = tableRow1.insertCell(1);
-    //     let newCell3 = tableRow1.insertCell(2);
-    //     let newCell4 = tableRow1.insertCell(3);
-    //     let newCell5 = tableRow1.insertCell(4);
-    //     let newCell6 = tableRow1.insertCell(5);
-    //     let newCell7 = tableRow1.insertCell(6);
-    //     let newCell8 = tableRow1.insertCell(7);
-    //     let newCell9 = tableRow1.insertCell(8);
-    //     let newCell10 = tableRow1.insertCell(9);
-    //     let peopleId = document.createTextNode(filteredPeople[i].id);
-    //     let firstNames = document.createTextNode(filteredPeople[i].firstName);
-    //     let lastNames =document.createTextNode(filteredPeople[i].lastName);
-    //     let personGender = document.createTextNode(filteredPeople[i].gender);
-    //     let personAge = document.createTextNode(filteredPeople[i].dob);
-    //     let personHeight =document.createTextNode(filteredPeople[i].height);
-    //     let personWeight =document.createTextNode(filteredPeople[i].weight);
-    //     let personEyeColor =document.createTextNode(filteredPeople[i].eyeColor);
-    //     let personParent =document.createTextNode(filteredPeople[i].parents);
-    //     let spouse =document.createTextNode(filteredPeople[i].currentSpouse);
+   function replaceTable(){
+       for(let i=0;i<people.length ;i++){
+        
+        document.getElementById("myTable").deleteRow(1);
+       }
+    function addNewRow(){
+     
+        for (let i=0; i<filteredPeople.length; i++){
+        let tableRef = document.getElementById("myTable");
+        let tableRow1 = tableRef.insertRow(1);
+        let newCell1 = tableRow1.insertCell(0);
+        let newCell2 = tableRow1.insertCell(1);
+        let newCell3 = tableRow1.insertCell(2);
+        let newCell4 = tableRow1.insertCell(3);
+        let newCell5 = tableRow1.insertCell(4);
+        let newCell6 = tableRow1.insertCell(5);
+        let newCell7 = tableRow1.insertCell(6);
+        let newCell8 = tableRow1.insertCell(7);
+        let newCell9 = tableRow1.insertCell(8);
+        let newCell10 = tableRow1.insertCell(9);
+        let peopleId = document.createTextNode(filteredPeople[i].id);
+        let firstNames = document.createTextNode(filteredPeople[i].firstName);
+        let lastNames =document.createTextNode(filteredPeople[i].lastName);
+        let personGender = document.createTextNode(filteredPeople[i].gender);
+        let personAge = document.createTextNode(filteredPeople[i].dob);
+        let personHeight =document.createTextNode(filteredPeople[i].height);
+        let personWeight =document.createTextNode(filteredPeople[i].weight);
+        let personEyeColor =document.createTextNode(filteredPeople[i].eyeColor);
+        let personParent =document.createTextNode(filteredPeople[i].parents);
+        let spouse =document.createTextNode(filteredPeople[i].currentSpouse);
         
 
         
-    //     newCell1.appendChild(peopleId);
-    //     newCell2.appendChild(firstNames);
-    //     newCell3.appendChild(lastNames);
-    //     newCell4.appendChild(personGender);
-    //     newCell5.appendChild(personAge);
-    //     newCell6.appendChild(personHeight);
-    //     newCell7.appendChild(personWeight);
-    //     newCell8.appendChild(personEyeColor);
-    //     newCell9.appendChild(personParent);
-    //     newCell10.appendChild(spouse);
-    //     }
-    // }
-    // addRow();
+        newCell1.appendChild(peopleId);
+        newCell2.appendChild(firstNames);
+        newCell3.appendChild(lastNames);
+        newCell4.appendChild(personGender);
+        newCell5.appendChild(personAge);
+        newCell6.appendChild(personHeight);
+        newCell7.appendChild(personWeight);
+        newCell8.appendChild(personEyeColor);
+        newCell9.appendChild(personParent);
+        newCell10.appendChild(spouse);
+        }
+    }
+addNewRow();
+}
+replaceTable();
 }
     // for(let i=0;i<filteredPeople.length;i++){
     //     console.log(filteredPeople["firstName"]);
