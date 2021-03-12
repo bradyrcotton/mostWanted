2477763ccdc1caf0e21fa56daa1a382905191f3d
unknown <bradyrcotton@gmail.com>
2021-03-12 13:30:43 -0600

    added forms for Id, Gender, DOB, Height, Weight, Eye Color to index.html

--- app.js
+++ app.js
@@ -43,12 +43,14 @@
 
 function searchByName(){
     // Grabbing the values from our nameForm form and inputs.
+   
     let firstNameInput = document.forms['nameForm']['fname'].value;
     let lastNameInput = document.forms['nameForm']['lname'].value;
+    let idInput = document.forms['nameForm']['peopleId'].value
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
-    let continueLoopPeople = true;
+    
     let filteredPeople = people.filter(function (person) {
-        while(person.firstName === firstNameInput && person.lastName === lastNameInput){
+        while(person.firstName === firstNameInput && person.lastName === lastNameInput && person.id === idInput){
             return true;
         }
         if(person.firstName === firstNameInput || person.lastName === lastNameInput){
@@ -56,47 +58,55 @@ function searchByName(){
         }
         else{ return false;}
     });
-    function addRow(){
-        console.log(filteredPeople);
-        for (let i=0; i<filteredPeople.length; i++){
-        let tableRef = document.getElementById("myTable");
-        let tableRow1 = tableRef.insertRow(1);
-        let newCell1 = tableRow1.insertCell(0);
-        let newCell2 = tableRow1.insertCell(1);
-        let newCell3 = tableRow1.insertCell(2);
-        let newCell4 = tableRow1.insertCell(3);
-        let newCell5 = tableRow1.insertCell(4);
-        let newCell6 = tableRow1.insertCell(5);
-        let newCell7 = tableRow1.insertCell(6);
-        let newCell8 = tableRow1.insertCell(7);
-        let newCell9 = tableRow1.insertCell(8);
-        let newCell10 = tableRow1.insertCell(9);
-        let peopleId = document.createTextNode(filteredPeople[i].id);
-        let firstNames = document.createTextNode(filteredPeople[i].firstName);
-        let lastNames =document.createTextNode(filteredPeople[i].lastName);
-        let personGender = document.createTextNode(filteredPeople[i].gender);
-        let personAge = document.createTextNode(filteredPeople[i].dob);
-        let personHeight =document.createTextNode(filteredPeople[i].height);
-        let personWeight =document.createTextNode(filteredPeople[i].weight);
-        let personEyeColor =document.createTextNode(filteredPeople[i].eyeColor);
-        let personParent =document.createTextNode(filteredPeople[i].parents);
-        let spouse =document.createTextNode(filteredPeople[i].currentSpouse);
+
+    // for(let i=0;i < filteredPeople.length;i++){
+    //     if (tableRef[i] != filteredPeople[i]){
+
+        
+    //     document.getElementById("myTable").deleteRow(i);
+    //     }
+    // }
+    // function addRow(){
+    //     console.log(filteredPeople);
+    //     for (let i=0; i<filteredPeople.length; i++){
+    //     let tableRef = document.getElementById("myTable");
+    //     let tableRow1 = tableRef.insertRow(1);
+    //     let newCell1 = tableRow1.insertCell(0);
+    //     let newCell2 = tableRow1.insertCell(1);
+    //     let newCell3 = tableRow1.insertCell(2);
+    //     let newCell4 = tableRow1.insertCell(3);
+    //     let newCell5 = tableRow1.insertCell(4);
+    //     let newCell6 = tableRow1.insertCell(5);
+    //     let newCell7 = tableRow1.insertCell(6);
+    //     let newCell8 = tableRow1.insertCell(7);
+    //     let newCell9 = tableRow1.insertCell(8);
+    //     let newCell10 = tableRow1.insertCell(9);
+    //     let peopleId = document.createTextNode(filteredPeople[i].id);
+    //     let firstNames = document.createTextNode(filteredPeople[i].firstName);
+    //     let lastNames =document.createTextNode(filteredPeople[i].lastName);
+    //     let personGender = document.createTextNode(filteredPeople[i].gender);
+    //     let personAge = document.createTextNode(filteredPeople[i].dob);
+    //     let personHeight =document.createTextNode(filteredPeople[i].height);
+    //     let personWeight =document.createTextNode(filteredPeople[i].weight);
+    //     let personEyeColor =document.createTextNode(filteredPeople[i].eyeColor);
+    //     let personParent =document.createTextNode(filteredPeople[i].parents);
+    //     let spouse =document.createTextNode(filteredPeople[i].currentSpouse);
         
 
         
-        newCell1.appendChild(peopleId);
-        newCell2.appendChild(firstNames);
-        newCell3.appendChild(lastNames);
-        newCell4.appendChild(personGender);
-        newCell5.appendChild(personAge);
-        newCell6.appendChild(personHeight);
-        newCell7.appendChild(personWeight);
-        newCell8.appendChild(personEyeColor);
-        newCell9.appendChild(personParent);
-        newCell10.appendChild(spouse);
-        }
-    }
-    addRow();
+    //     newCell1.appendChild(peopleId);
+    //     newCell2.appendChild(firstNames);
+    //     newCell3.appendChild(lastNames);
+    //     newCell4.appendChild(personGender);
+    //     newCell5.appendChild(personAge);
+    //     newCell6.appendChild(personHeight);
+    //     newCell7.appendChild(personWeight);
+    //     newCell8.appendChild(personEyeColor);
+    //     newCell9.appendChild(personParent);
+    //     newCell10.appendChild(spouse);
+    //     }
+    // }
+    // addRow();
 }
     // for(let i=0;i<filteredPeople.length;i++){
     //     console.log(filteredPeople["firstName"]);
